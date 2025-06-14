4b0f950a971a994f48187c62893cf639504cbf48
Asa Sprow <asasprow@gmail.com>
2025-06-14 13:38:05 -0600

    show hidden files by default (hide gitignored)

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -20,6 +20,19 @@ return {
           ['\\'] = 'close_window',
         },
       },
+      filtered_items = {
+        visible = true,
+        show_hidden_count = true,
+        hide_dotfiles = false,
+        hide_gitignored = true,
+        hide_by_name = {
+          -- add extension names you want to explicitly exclude
+          -- '.git',
+          -- '.DS_Store',
+          -- 'thumbs.db',
+        },
+        never_show = {},
+      },
     },
   },
 }
