eea07064c40020ae2624f6117bcfdae1f127295b
Asa Sprow <asasprow@gmail.com>
2025-08-04 23:12:49 -0600

    remove fyler, add overlook and quick keybinds for write and quit

--- lua/custom/plugins/fyler.lua
+++ lua/custom/plugins/fyler.lua
@@ -1,16 +1,17 @@
-return {
-  'A7Lavinraj/fyler.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
-  branch = 'stable',
-  opts = {
-    icon_provider = 'nvim-web-devicons',
-    close_on_select = false,
-    confirm_simple = false,
-    views = {
-      explorer = {
-        kind = 'split:leftmost',
-        width = 0.2,
-      },
-    },
-  },
-}
+return {}
+-- return {
+--   'A7Lavinraj/fyler.nvim',
+--   dependencies = { 'nvim-tree/nvim-web-devicons' },
+--   branch = 'stable',
+--   opts = {
+--     icon_provider = 'nvim-web-devicons',
+--     close_on_select = false,
+--     confirm_simple = false,
+--     views = {
+--       explorer = {
+--         kind = 'split:leftmost',
+--         width = 0.2,
+--       },
+--     },
+--   },
+-- }
