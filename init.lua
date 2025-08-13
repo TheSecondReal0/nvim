bb8d96342ff0c56b1b0abdf41a17e00245d7212f
Asa Sprow <asasprow@gmail.com>
2025-08-12 21:53:20 -0600

    map space to nop before setting leader

--- init.lua
+++ init.lua
@@ -87,6 +87,8 @@ P.S. You can delete this when you're done too. It's your config now! :)
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+-- Set space to do nothing so vim knows you're not using it as movement
+vim.keymap.set('n', '<Space>', '<Nop>', { silent = true, remap = false })
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
