03c294f4d8aa4517c44eef8a1f846ab6bafff7ca
Asa Sprow <asasprow@gmail.com>
2026-02-02 19:19:52 -0700

    add diagnostics to hover providers

--- lua/custom/plugins/hover.lua
+++ lua/custom/plugins/hover.lua
@@ -10,7 +10,7 @@ return {
         -- require("hover.providers.jira")
         -- require("hover.providers.dap")
         -- require("hover.providers.fold_preview")
-        -- require("hover.providers.diagnostic")
+        require 'hover.providers.diagnostic'
         -- require("hover.providers.man")
         -- require("hover.providers.dictionary")
         -- require("hover.providers.highlight")
@@ -29,12 +29,8 @@ return {
     -- Keymaps
     vim.keymap.set('n', 'K', require('hover').hover, { desc = 'hover.nvim' })
     vim.keymap.set('n', 'gK', require('hover').hover_select, { desc = 'hover.nvim (select)' })
-    vim.keymap.set('n', '<C-p>', function()
-      require('hover').hover_switch 'previous'
-    end, { desc = 'hover.nvim (previous source)' })
-    vim.keymap.set('n', '<C-n>', function()
-      require('hover').hover_switch 'next'
-    end, { desc = 'hover.nvim (next source)' })
+    vim.keymap.set('n', '<C-p>', function() require('hover').hover_switch 'previous' end, { desc = 'hover.nvim (previous source)' })
+    vim.keymap.set('n', '<C-n>', function() require('hover').hover_switch 'next' end, { desc = 'hover.nvim (next source)' })
 
     -- Mouse support
     vim.keymap.set('n', '<MouseMove>', require('hover').hover_mouse, { desc = 'hover.nvim (mouse)' })
